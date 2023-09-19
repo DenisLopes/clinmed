9aee8906f98fc69468e12904ee62777fca626fd5
Denis Carlos Lopes da Silva <denisds@ciandt.com>
2023-09-19 17:20:50 -0300

    Criando os DTO da classe medico

---
+++ src/main/java/clin/med/api/endereco/DadosEndereco.java
@@ -0,0 +1,11 @@
+package clin.med.api.endereco;
+
+public record DadosEndereco(
+        String logradouro,
+        String bairro,
+        String cep,
+        String cidade,
+        String uf,
+        String complemento,
+        String numero) {
+}
---
+++ src/main/java/clin/med/api/medico/DadosCadastroMedico.java
@@ -0,0 +1,11 @@
+package clin.med.api.medico;
+
+import clin.med.api.endereco.DadosEndereco;
+
+public record DadosCadastroMedico(
+        String nome,
+        String email,
+        String crm,
+        Especialidade especialidade,
+        DadosEndereco endereco) {
+}
