f47dc07a642183d2f0de1a34c9ce813eb48fd3ea
DenisLopes <denislopes80@gmail.com>
2023-09-18 23:40:54 -0300

    first commit

---
+++ src/main/java/clin/med/api/ClinmedApplication.java
@@ -0,0 +1,13 @@
+package clin.med.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinmedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinmedApplication.class, args);
+	}
+
+}
