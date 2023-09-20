17e32557edc6a5ddfb599a4192675ed40cb703f8
Denis Carlos Lopes da Silva <denisds@ciandt.com>
2023-09-20 16:37:12 -0300

    Mapeado a Classe medico com JPA

---
+++ src/main/java/clin/med/api/medico/Medico.java
@@ -0,0 +1,30 @@
+package clin.med.api.medico;
+
+import clin.med.api.endereco.Endereco;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+@Table(name = "medicos")
+@Entity(name = "medico")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Medico {
+
+    @Id
+    private Long id;
+    private String nome;
+    private String email;
+    private String crm;
+
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+
+    @Embedded
+    private Endereco endereco;
+}
